368354e1a33d1dc6b8aaee0e75c30667762febea
崮生 <2234839456@qq.com>
2017-09-28 17:15:11 +0800

    Update 2017-9-28方法-重载.java

--- 2017-9-28方法-重载.java
+++ 2017-9-28方法-重载.java
@@ -31,10 +31,52 @@ public class aa {
 			System.out.print("\n");
 		}
 	}
+	public static int digui(int n){
+		if(n==1)
+			return 1;
+		return (n*(digui(n-1)));
+	}
+	
+	public static void shuzu() {//数组操作
+		int[] arr={2,412,45,25,1,34};
+		zuixiaozhi(arr);
+		maopao(arr);
+		dayin(arr);
+	}
+	public static void zuixiaozhi(int[] arr) {//最小值
+		int temp=arr[0];
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]<temp)
+				temp=arr[i];
+		}
+		System.out.println("arr 的最小值:"+temp);
+	}
+	
+	public static void dayin(int[] arr) {//打印
+		for(int i=0;i<arr.length;i++){
+			System.out.println("arr["+i+"]:"+arr[i]);
+		}
+	}
+	
+	public static void maopao(int[] arr) {//冒泡排序
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=0;j<arr.length-i-1;j++){
+				if(arr[j]>arr[j+1]){
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println( sz(1,'+',2));
 		cf99(11);
+		System.out.println("递归："+ digui(4));
+		shuzu();
+		
+		
 	}
 
 }
